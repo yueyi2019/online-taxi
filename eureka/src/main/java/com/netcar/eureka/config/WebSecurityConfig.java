fa8ad572da2dced8fbd83ebeebc0066d7467b3e8
yueyi <yueyi2019@qq.com>
2019-09-04 22:20:34 +0800

    eureka 注册中心集群搭建

---
+++ eureka/src/main/java/com/netcar/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.netcar.eureka.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// 关闭csrf
+		http.csrf().disable(); 
+		// 开启认证
+		http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); 
+	}
+}
