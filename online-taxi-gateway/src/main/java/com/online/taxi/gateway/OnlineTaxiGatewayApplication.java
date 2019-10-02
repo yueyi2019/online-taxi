e9eca4470ec67955d89ab75a8087a3508a832faf
yueyi <yueyi2019@qq.com>
2019-10-02 10:43:26 +0800

    网关过滤器配置：直接转第三方网站，不修饰的转自己服务

--- online-taxi-gateway/src/main/java/com/online/taxi/gateway/OnlineTaxiGatewayApplication.java
+++ online-taxi-gateway/src/main/java/com/online/taxi/gateway/OnlineTaxiGatewayApplication.java
@@ -2,10 +2,10 @@ package com.online.taxi.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableZuulProxy
+@EnableEurekaClient
 public class OnlineTaxiGatewayApplication {
 
 	public static void main(String[] args) {
