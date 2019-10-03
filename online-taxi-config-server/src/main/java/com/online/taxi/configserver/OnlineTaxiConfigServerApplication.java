161cca05b780c003494c58da4042d5f272898091
yueyi <yueyi2019@qq.com>
2019-10-03 20:13:31 +0800

    config-server完成

---
+++ online-taxi-config-server/src/main/java/com/online/taxi/configserver/OnlineTaxiConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.online.taxi.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class OnlineTaxiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTaxiConfigServerApplication.class, args);
+	}
+
+}
---
+++ online-taxi-config-server/src/test/java/com/online/taxi/configserver/OnlineTaxiConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.online.taxi.configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineTaxiConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
