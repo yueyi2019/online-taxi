b994608f67a15acc4f95bf486ca6deaa30472eb9
yueyi <yueyi2019@qq.com>
2019-10-13 12:09:05 +0800

    sha1 加密工具类

---
+++ online-taxi-common/src/main/java/com/online/taxi/common/util/Sha1Util.java
@@ -0,0 +1,24 @@
+package com.online.taxi.common.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author yueyi2019
+ */
+public class Sha1Util {
+
+    /**
+     * 将输入源 转换成 密文
+     * @param src
+     * @return
+     */
+    public static String encode(String src) {
+        if (StringUtils.isEmpty(src)) {
+            return null;
+        } else {
+            return DigestUtils.sha1Hex(src);
+
+        }
+    }
+}
