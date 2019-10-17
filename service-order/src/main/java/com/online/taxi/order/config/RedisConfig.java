d9a6d434f282baf52058148a4d0288833bfcccc6
yueyi <yueyi2019@qq.com>
2019-10-17 08:38:22 +0800

    redlock 实现

--- service-order/src/main/java/com/online/taxi/order/config/RedisConfig.java
+++ service-order/src/main/java/com/online/taxi/order/config/RedisConfig.java
@@ -1,10 +1,10 @@
 package com.online.taxi.order.config;
 
 import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,16 +15,38 @@ public class RedisConfig {
 
     @Autowired
     RedisSentinelProperties properties;
+    //以下为redisson锁
+//    @Bean(name = "redisson")
+//    @Order(1)
+//    public Redisson getRedisson(){
+//
+//        Config config = new Config();
+//        config.useSentinelServers()
+//                .setMasterName(properties.getMasterName())
+//                .addSentinelAddress(properties.getAddress())
+//                .setDatabase(0);
+//        return (Redisson) Redisson.create(config);
+//    }
+    //以上为redisson锁
 
-    @Bean(name = "redisson")
-    @Order(1)
-    public Redisson getRedisson(){
-
+    //以下为红锁
+    @Bean
+    public RedissonClient redissonRed1(){
         Config config = new Config();
-        config.useSentinelServers()
-                .setMasterName(properties.getMasterName())
-                .addSentinelAddress(properties.getAddress())
-                .setDatabase(0);
-        return (Redisson) Redisson.create(config);
+        config.useSingleServer().setAddress("127.0.0.1:6379").setDatabase(0);
+        return Redisson.create(config);
     }
+    @Bean
+    public RedissonClient redissonRed2(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("127.0.0.1:6380").setDatabase(0);
+        return Redisson.create(config);
+    }
+    @Bean
+    public RedissonClient redissonRed3(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("127.0.0.1:6381").setDatabase(0);
+        return Redisson.create(config);
+    }
+    //以上为红锁
 }
