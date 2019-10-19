0e6fd87fde1e6da0c7536aa57ff2256dd83b4661
yueyi <yueyi2019@qq.com>
2019-10-19 14:19:14 +0800

    common 增加 司机抢单redis前缀

--- online-taxi-common/src/main/java/com/online/taxi/common/constant/RedisKeyConstant.java
+++ online-taxi-common/src/main/java/com/online/taxi/common/constant/RedisKeyConstant.java
@@ -9,4 +9,9 @@ public class RedisKeyConstant {
      * 抢单key前缀
      */
     public static final String GRAB_LOCK_ORDER_KEY_PRE = "lock_";
+
+    /**
+     * 司机抢单前缀
+     */
+    public static final String DRIVER_LISTEN_ORDER_PRE = "driver_order_list_";
 }
