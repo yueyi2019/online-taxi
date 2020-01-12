78d25281ea223bbb87e07e39e48dc813dcce0006
yueyi <yueyi2019@qq.com>
2020-01-12 22:36:50 +0800

    第一课，例子

--- eureka/src/main/java/com/online/taxi/eureka/config/WebSecurityConfig.java
+++ eureka/src/main/java/com/online/taxi/eureka/config/WebSecurityConfig.java
@@ -12,7 +12,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// 关闭csrf
-		http.csrf().disable(); 
+//		http.csrf().disable(); 
+		/*
+		 * 默认情况下添加SpringSecurity依赖的应用每个请求都需要添加CSRF token才能访问，Eureka客户端注册时并不会添加，所以需要配置/eureka/**路径不需要CSRF token。
+		 */
+		http.csrf().ignoringAntMatchers("/eureka/**");
 		// 开启认证支持HttpBasic
 		http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); 
 	}
