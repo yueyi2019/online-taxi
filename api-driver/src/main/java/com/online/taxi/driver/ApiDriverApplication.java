5a47a0fb505b083a235ef36b8d3c5d02637626b3
yueyi <yueyi2019@qq.com>
2020-01-17 21:18:17 +0800

    司机ribbon，乘客feign，改造

--- api-driver/src/main/java/com/online/taxi/driver/ApiDriverApplication.java
+++ api-driver/src/main/java/com/online/taxi/driver/ApiDriverApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.web.client.RestTemplate;
+
+import com.online.taxi.driver.annotation.ExcudeRibbonConfig;
+import com.online.taxi.driver.ribbonconfig.RibbonConfiguration;
 
 /**
  * @author yueyi2019
@@ -16,6 +21,11 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableCircuitBreaker
+@ComponentScan(excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.ANNOTATION,value=ExcudeRibbonConfig.class)
+})
+// 下面是让所有client都实现随机策略
+//@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class ApiDriverApplication {
 
 	public static void main(String[] args) {
--- api-driver/src/main/java/com/online/taxi/driver/ribbonconfig/RibbonConfiguration.java
+++ api-driver/src/main/java/com/online/taxi/driver/ribbonconfig/RibbonConfiguration.java
@@ -2,20 +2,26 @@ package com.online.taxi.driver.ribbonconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+import com.netflix.loadbalancer.ILoadBalancer;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
+import com.online.taxi.driver.annotation.ExcudeRibbonConfig;
 /**
  * 该类不应该在主应用程序的扫描之下，需要修改启动类的扫描配置。
  * 
  * @author yueyi2019
  *
  */
-//@Configuration
+@Configuration
+@ExcudeRibbonConfig
 public class RibbonConfiguration {
 
 	@Bean
 	public IRule ribbonRule() {
 		return new RandomRule();
 	}
+	
+	
 }
