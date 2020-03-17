d8038f8127b268954aae76d61da67c26f9617732
yueyi <yueyi2019@qq.com>
2020-03-17 23:54:21 +0800

    消息监听 异常处理

--- service-jms-consumer/src/main/java/com/online/taxi/jms/consumer/component/ConsumerQueue.java
+++ service-jms-consumer/src/main/java/com/online/taxi/jms/consumer/component/ConsumerQueue.java
@@ -13,19 +13,20 @@ import javax.jms.TextMessage;
 @Component
 public class ConsumerQueue {
     /**
-     * 使用JmsListener配置消费者监听的队列，其中text是接收到的消息
+     * 	使用JmsListener配置消费者监听的队列，其中text是接收到的消息
      * @param text
      */
    @JmsListener(destination = "1",containerFactory = "jmsListenerContainerQueue")
    public void receiveQueue1(TextMessage text, Session session) throws JMSException {
        try {
-           int i = 1/0;
+//           int i = 1/0;
            System.out.println("消費者收到的queue1报文为:"+text.getText());
-           //提交完事务后，再确认。
-
+           
+           //提交完事务后，再确认。因为哪怕下次有事件再来，插库会失败。
+           text.acknowledge();
 
        }catch (Exception e){
-//            session.recover();
+            session.recover();
            try {
                Thread.sleep(5000);
            } catch (InterruptedException e1) {
@@ -33,7 +34,7 @@ public class ConsumerQueue {
            }
            System.out.println("异常了");
        }finally {
-           text.acknowledge();
+           
        }
 
    }
